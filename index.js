913a24420d4e4e6be59e1db1a1186be37bc17cc8
Danny Ramasawmy <dannyramasawmy@gmail.com>
2022-06-02 12:08:17 +0100

    working on laptop

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 // get mesh coordinates from canvas element
-let {X, Y, gridSpacing} = MeshGridFromCanvas(canvasId, 25);
+let {X, Y, gridSpacing} = MeshGridFromCanvas(canvasId, 50);
 
 // generate a random matrix array
 let V = RandomMatrix(X.NumRows, X.NumCols);
+let V2 = RandomMatrix(X.NumRows, X.NumCols);
 
 // plot
 
 GridPlot(canvasId, X, Y, V, gridSpacing, SquareMaker);
-GridPlot(canvasId, X, Y, V, gridSpacing, CircleMaker);
+GridPlot(canvasId, X, Y, V2, gridSpacing, CircleMaker);
